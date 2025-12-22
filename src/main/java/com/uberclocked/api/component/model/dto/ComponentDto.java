4ccadedd4dafcefd2dc9bceb39ba9f50416b16f9
GarroteSantiago <sgarrote@mail.austral.edu.ar>
2025-12-22 13:40:33 -0300

    Annotated the dtos field to make proper validations

--- src/main/java/com/uberclocked/api/component/model/dto/ComponentDto.java
+++ src/main/java/com/uberclocked/api/component/model/dto/ComponentDto.java
@@ -1,11 +1,14 @@
 package com.uberclocked.api.component.model.dto;
 
 import java.util.Set;
-
 import com.uberclocked.api.component.model.dto.field.ComponentFieldDto;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+
 public record ComponentDto(
-    String code,
-    String displayName,
-    Set<ComponentFieldDto> fields) {
+    @NotBlank String code,
+    @NotBlank String displayName,
+    @NotEmpty @Valid Set<ComponentFieldDto> fields) {
 }
--- src/main/java/com/uberclocked/api/component/model/dto/field/ComponentFieldDto.java
+++ src/main/java/com/uberclocked/api/component/model/dto/field/ComponentFieldDto.java
@@ -2,10 +2,12 @@ package com.uberclocked.api.component.model.dto.field;
 
 import com.uberclocked.api.component.model.entity.field.FieldType;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
 public record ComponentFieldDto(
-    Long id,
-    String name,
-    FieldType type,
+    @NotBlank String name,
+    @NotNull FieldType type,
     boolean required,
     String defaultValue) {
 }
