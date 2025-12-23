d27eaad99e1f1c1d06c55aab3f484801e4887ba2
GarroteSantiago <sgarrote@mail.austral.edu.ar>
2025-12-23 00:30:46 -0300

    Implemented service with create method

---
+++ src/main/java/com/uberclocked/api/component/repository/ComponentRepository.java
@@ -0,0 +1,11 @@
+package com.uberclocked.api.component.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.uberclocked.api.component.model.entity.Component;
+
+@Repository
+public interface ComponentRepository extends JpaRepository<Component, String> {
+  boolean existByCode(String code);
+}
--- src/main/java/com/uberclocked/api/component/service/ComponentService.java
+++ src/main/java/com/uberclocked/api/component/service/ComponentService.java
@@ -2,15 +2,24 @@ package com.uberclocked.api.component.service;
 
 import org.springframework.stereotype.Service;
 
+import com.uberclocked.api.common.exceptions.ResourceAlreadyExistsException;
+import com.uberclocked.api.component.mapper.ComponentMapper;
 import com.uberclocked.api.component.model.dto.ComponentDto;
+import com.uberclocked.api.component.repository.ComponentRepository;
 
 import jakarta.transaction.Transactional;
 
 @Service
 @Transactional
 public final class ComponentService {
+  private ComponentRepository repository;
+  private ComponentMapper mapper;
+
   public ComponentDto create(ComponentDto dto) {
-    // TODO
-    throw new UnsupportedOperationException("Not implemented yet.");
+    if (repository.existByCode(dto.code())) {
+      throw new ResourceAlreadyExistsException(
+          "Component with code '" + dto.code() + "' already exists.");
+    }
+    return mapper.toDto(repository.save(mapper.toEntity(dto)));
   }
 }
